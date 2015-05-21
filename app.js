6b8aa4f63a45ccc47f39ebe4b7c0cb71bb83efd7
yyy <super.doter.yyy@gmail.com>
2015-05-21 17:35:51 +0800

    fix

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
+var path = require("path");
+var Request = require("request");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.all("/", function(req, res){
+	
+	var url = (req.query&&req.query.url) || "";
+	console.log("清楚cdn缓存地址"+url);
+
+	Request("http://workflow.dp/api/v0.1/cdn/refresh/h5?url="+url, function (error, response, body) {
+		res.send(response);
+		res.send(body);	
+		res.end();
+	})
+});
+
+app.listen(9567);
+
+console.log("端口:9567已启动");
+

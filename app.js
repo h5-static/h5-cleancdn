0d088be391a5e1f364a00b6b07b9b39f48c9d71e
yyy <super.doter.yyy@gmail.com>
2015-05-22 11:28:22 +0800

    fix

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ app.use(cookieParser());
 app.all("/", function(req, res){
 	
 	var url = (req.query&&req.query.url) || "";
-	console.log("清楚cdn缓存地址"+url);
-	Request("http://workflow.dp/api/v0.1/cdn/refresh/h5?url="+encodeURIComponent(url), function (error, response, body) {
+	// console.log("清楚cdn缓存地址"+url);
+	request.post('http://workflow.dp/api/v0.1/cdn/refresh/h5', {form:{url:url}},, function (error, response, body) {
 		console.log(body);	
-	})
+	});
+
 	res.end();
 });
 

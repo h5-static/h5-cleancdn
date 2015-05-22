9453b9ef58fc774f89246763ee9705018b520cef
yyy <super.doter.yyy@gmail.com>
2015-05-22 11:22:28 +0800

    fix

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ app.all("/", function(req, res){
 	
 	var url = (req.query&&req.query.url) || "";
 	console.log("清楚cdn缓存地址"+url);
-
-	Request("http://workflow.dp/api/v0.1/cdn/refresh/h5?url="+url, function (error, response, body) {
+	Request("http://workflow.dp/api/v0.1/cdn/refresh/h5?url="+encodeURIComponent(url), function (error, response, body) {
 		console.log(body);	
 	})
 	res.end();
